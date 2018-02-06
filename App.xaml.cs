f887dfaca57491d4cdf95630918191997d2ca715
Montoya Edu <montoya.edu@gmail.com>
2018-02-06 14:23:06 +0100

    Initial commit.

---
+++ App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Xamarin.Forms;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace Ethic1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Ethic1Page();
+        }
+
+        protected override void OnStart()
+        {
+            AppCenter.Start(
+                  "ios=96997c18-01be-4693-9aca-9ecf59492840;" 
+                + "uwp=e320f61f-24c5-414c-9d5b-53a4ca811eb6;" 
+                + "android=7a2127b6-5796-4027-bc8c-51875a5325a9"
+                , typeof(Analytics)
+                , typeof(Crashes));
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ Ethic1Page.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Ethic1
+{
+    public partial class Ethic1Page : ContentPage
+    {
+        public Ethic1Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
