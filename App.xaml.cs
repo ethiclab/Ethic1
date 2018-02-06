9819c65a7fdbe2632a30ac58f4570aea67525ca1
Montoya Edu <montoya.edu@gmail.com>
2018-02-06 18:36:18 +0100

    Remove push.

--- App.xaml.cs
+++ App.xaml.cs
@@ -23,7 +23,6 @@ namespace Ethic1
                 + "android=7a2127b6-5796-4027-bc8c-51875a5325a9"
                 , typeof(Analytics)
                 , typeof(Crashes)
-                , typeof(Push)
             );
         }
 
